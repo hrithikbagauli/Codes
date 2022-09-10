fbdf7be6beeca4256acfa878c569379e4c3a2f8a
Hrithik Bagauli <hrithikbagauli@gmail.com>
2022-09-10 20:10:02 +0530

    DSA

---
+++ Practice.java
@@ -0,0 +1,11 @@
+package DSA;
+
+import java.util.HashMap;
+
+class Practice{
+    public static void main(String[] args) {
+        for(char i=97; i<=122; i++){
+            System.out.println(i);
+        }
+    }
+}
